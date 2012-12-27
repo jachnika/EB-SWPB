3e2066151d103f9ed3f5e7cd0f1fc34b6a506529
jachnika <arkadiusz.jachnik@gmail.com>
2012-12-27 18:16:02 +0100

    Zaprojektowane GUI
    Zrobione okna w modelu MVC

---
+++ src/eb/swpb/controller/EBSWPB.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eb.swpb.controller;
+
+/**
+ *
+ * @author jachnika
+ */
+public class EBSWPB {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
